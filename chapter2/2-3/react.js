858f44fa314eb1bf903b845597d94e07f73e67eb
min-kyou <sho9uttot1helo8rd@gmail.com>
2022-06-16 12:23:09 +0900

    2-3-1途中

---
+++ chapter2/2-3/react.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+    data() {
+        return {
+            current: new Date()
+        };
+    },
+
+    created() {
+        this.timer = setInterval(() => {
+            this.current = new Date();
+        }, 1000)
+    },
+
+    beforeUnmount() {
+        clearInterval(this.timer);
+    }
+}).mount('#app');
