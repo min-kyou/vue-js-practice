b24b03a695e808d4ec973c9a1468cc9e48aacc02
min-kyou <sho9uttot1helo8rd@gmail.com>
2022-07-06 21:12:09 +0900

    3-2-5途中

---
+++ chapter3/3-2/model_obj.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+    data() {
+        return {
+            unit: {}
+        };
+    },
+
+    methods: {
+        onchange() {
+            console.log(`${this.unit.name}：${this.unit.size}`);
+        }
+    }
+
+}).mount('#app');
